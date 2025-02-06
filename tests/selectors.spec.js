e1227a83fdb5e1f270223a3c192d5477dac49e0c
José Quiñónez <jquinonez@tsc.com.gt>
2025-02-06 06:27:36 -0600

    Agregar pruebas de Playwright y archivo HTML interactivo

---
+++ tests/selectors.spec.js
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+
+test.only("Learning selectors", async ({ page }) => {
+  await page.goto('http://127.0.0.1:5500/clickMe.html')
+
+  // 1. Select by Id
+  await page.locator('#clickButton').click();
+
+  // 2. Select by Class
+  await page.locator('.button-style').click();
+
+  // 3. Select by Tag
+  await page.locator('button').click();
+
+  // 5. Select by Role
+  await page.locator('button[role="button"]').click();
+
+  // 6. Select by Attribute
+  await page.locator('[data-action="increment"]').click();
+
+  // 7. Select by Css
+  await page.locator('div[data-counter="number"]').click();
+
+  await page.pause();
+});
